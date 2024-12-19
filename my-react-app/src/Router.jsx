a612fe619ec1ebb4be3c43c6b188cfe0d119d990
Karsten Walraet <karsten.walraet@gmail.com>
2024-12-19 15:25:05 +0100

    création des composants et répartition du css

--- my-react-app/src/Router.jsx
+++ my-react-app/src/Router.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import User from './pages/User';
+import Home from './pages/Home/Home';
+import SignIn from './pages/SignIn/SignIn';
+import User from './pages/User/User';
 
 const AppRouter = () => {
   return (
