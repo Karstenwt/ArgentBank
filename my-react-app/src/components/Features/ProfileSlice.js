54823687824583060ecd6bc88259372042083355
Karsten Walraet <karsten.walraet@gmail.com>
2024-12-31 16:26:38 +0100

    modification de createSlice

--- my-react-app/src/components/Features/ProfileSlice.js
+++ my-react-app/src/components/Features/ProfileSlice.js
@@ -5,3 +5,26 @@ const initialState = {
   isLoading: false,
   error: null,
 };
+const profileSlice = createSlice({
+  name: "profile",
+  initialState,
+  reducers: {
+    fetchProfileStart(state) {
+      state.isLoading = true;
+      state.error = null;
+    },
+    fetchProfileSuccess(state, action) {
+      state.isLoading = false;
+      state.data = action.payload;
+    },
+    fetchProfileFailure(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { fetchProfileStart, fetchProfileSuccess, fetchProfileFailure } =
+  profileSlice.actions;
+
+export default profileSlice.reducer;
